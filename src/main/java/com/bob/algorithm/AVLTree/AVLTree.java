59d2370d1be34d24b3811d4d047cb94b2f7f0f0e
bob <491889203@qq.com>
2019-01-07 22:24:50 +0800

    AVLTree

---
+++ src/main/java/com/bob/algorithm/AVLTree/AVLTree.java
@@ -0,0 +1,38 @@
+package com.bob.algorithm.AVLTree;
+
+public class AVLTree {
+
+	private AVLNode<Integer> rootNode;
+
+	public boolean insert(Integer i) {
+
+		AVLNode<Integer> insert = insert(i, rootNode);
+
+		return insert != null;
+	}
+
+	private AVLNode<Integer> insert(Integer i, AVLNode<Integer> r) {
+
+		if (r == null) r = new AVLNode<>(i);
+
+
+	}
+
+	private class AVLNode<T>{
+
+		public T element;
+		public AVLNode<T> leftNode;
+		public AVLNode<T> rightNode;
+		public int height;
+
+		public AVLNode(T element) {
+			this.element = element;
+		}
+
+		public AVLNode(T element, AVLNode<T> leftNode, AVLNode<T> rightNode) {
+			this.element = element;
+			this.leftNode = leftNode;
+			this.rightNode = rightNode;
+		}
+	}
+}
